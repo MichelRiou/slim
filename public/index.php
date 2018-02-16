9bdd275c952a757ed0051bf1137096a335454061
Michel RIOU <michel.riou@laposte.net>
2018-02-16 12:25:39 +0100

    slim suite

--- public/index.php
+++ public/index.php
@@ -12,10 +12,45 @@ $app = new App();
 
 //Définition d'une route
 $app->get(
-    "/hello",
-    function (Request $request, Response $response) {
-        return $response->getBody()->write("Hello");
-    });
+    "/hello/{firstName}/{name}/{month:[a-z]{3,}}-{year:\d{4}}",                       // ce qui est entre / et - est considéré comme {month}
+    function (Request $request, Response $response, array $args) {
+        $firstName = $args["firstName"];
+        $name = $args["name"];
+        $year = $args["year"];
+        $month = $args["month"];
+        if ($year > 2000) {
+            return $response->withRedirect("/redirect");
+        } else {
+            // $name=$request->getParam("name" ?? "world");
+            return $response->getBody()->write("Hello $firstName $name
+        vous êtes né en $year au mois de $month");
+        }
+    })->setName("hello");
+$app->get(
+    "/redirect", function (Request $request, Response $response) {
+    $response->getBody()->write("vous avez été redirigé");
+}
+);
+$app->get(
+    "/home", function(Request $resquest, Response $response){
+    $url=$this->get("router")->pathFor("hello",
+        ["firstName"=>"Tycho",
+        "name"=>"Brahé",
+        "month"=>"janvier",
+        "year"=>"1520"]
+        );
+    $link="<a href=$url>lien vers hello</a>";
+    $response->getBody()->write($link);
+}
+);
+$app->get("/products/all",
+    function (Request $request, Response $response){     // La function est le controleur qui récupère la route et la formatte
+        $data = [
+            ["code"=> "A5", "price"=> 12],
+["code"=> "B8", "price"=> 19]
+];
+return $response->withJson($data);
+});
 
 //Lancement du framwork
 $app->run();
